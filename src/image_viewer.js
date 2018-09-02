36a67d702064ee994d942a3f1fe79f336ece3488
antcin <ciniglioantonio@gmail.com>
2018-09-02 14:10:20 +0100

    create css and install css-loader and style-loader

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,3 +1,5 @@
+import "../styles/image_viewer.css"; // for anything non js, need to specify extension
+
 const image = document.createElement("img");
 image.src = "http://lorempixel.com/400/400";
 
