115399ffb9050418b5bea213a949f1d4e877db9d
antcin <ciniglioantonio@gmail.com>
2018-09-02 13:48:57 +0100

    import image from url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import sum from "./sum";
+import "./image_viewer"; // as we are not importing any executable code, we can simply import it like this. need to make sure it's imported otherwise it won't end up in webpack bundle
 
 const total = sum(10, 5);
 console.log(total);
