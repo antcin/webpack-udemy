547ba88beb01ed79390204e1da68a91c5460edb6
antcin <ciniglioantonio@gmail.com>
2018-09-04 22:12:24 +0100

    figure out publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"), //reference to directory the output file should be saved to. Gotcha: we must specify absolute filepath. Use node.s path module to generate path. We use path.resolve() to make sure that webpack.config can be safely executed on any OS. "build" is the folder where bundle.js is saved
-    filename: "bundle.js" // what webpack should call the actual file that gets created
+    filename: "bundle.js", // what webpack should call the actual file that gets created
+    publicPath: "build/"
   },
   module: {
     rules: [
