8afcbda352b57614b5beb293135d8853a42c800d
antcin <ciniglioantonio@gmail.com>
2018-09-05 21:01:33 +0100

    refactor image_viewer function to export image; import image_viewer using System.import

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,13 +1,9 @@
-import big from "../assets/big.jpg";
 import small from "../assets/small.jpg";
 import "../styles/image_viewer.css"; // for anything non js, need to specify extension
 
-const smallImage = document.createElement("img");
-smallImage.src = small;
+export default () => {
+  const smallImage = document.createElement("img");
+  smallImage.src = small;
 
-document.body.appendChild(smallImage);
-
-const bigImage = document.createElement("img");
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+  document.body.appendChild(smallImage);
+};
